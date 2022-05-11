6bfe2dab9f5d1af674cd05d5dff26c18a09e16b5
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-05-12 00:05:43 +0200

    Reached containerize()

--- installLeanMSR.sh
+++ installLeanMSR.sh
@@ -57,3 +57,76 @@ sourceSecrets(){
 }
 sourceSecrets
 
+mountImagesShare(){
+  logI "Mounting the given file share"
+  mkdir -p "$sd"
+  sudo mount -t cifs "$AZ_SMB_PATH" "$sd" -o "vers=3.0,username=$SAG_AZ_SA_NAME,password=$AZ_SM_SHARE_KEY,dir_mode=0777,file_mode=0777"
+  resultMount=$?
+  if [ $resultMount -ne 0 ]; then
+    logE "Error mounting the images share, result $resultMount"
+    exit 4
+  fi
+  logI "Creating work folder and assuring shared folders (${binDir})"
+  mkdir -p "${binDir}" "$wd" "$sd/sessions/$crtDay"
+  touch "${binDir}/lastMountTime"
+}
+mountImagesShare
+
+# TODO: current code always expects a fixes image, to add the option not to
+assureBinaries(){
+  if [ ! -f "${installerSharedBin}" ]; then
+    logE "Installer Binary must exist in the share: ${installerSharedBin}"
+    exit 1
+  fi
+  logI "Copying installer binary from the share"
+  cp "${installerSharedBin}" "${SUIF_INSTALL_INSTALLER_BIN}"
+  logI "Installer binary copied"
+
+
+  if [ ! -f "${sumBootstrapSharedBin}" ]; then
+    logE "SUM Bootstrap binary must exist in the share: ${sumBootstrapSharedBin}"
+    exit2
+  fi
+  logI "Copying sum bootstrap binary from the share"
+  cp "${sumBootstrapSharedBin}" "${SUIF_PATCH_SUM_BOOSTSTRAP_BIN}"
+
+  logI "Installer binary copied"
+  chmod u+x "${SUIF_INSTALL_INSTALLER_BIN}"
+  chmod u+x "${SUIF_PATCH_SUM_BOOSTSTRAP_BIN}"
+}
+assureBinaries
+
+assureSUM(){
+  export SUIF_SUM_HOME=/tmp/sumv11
+  mkdir -p "${SUIF_SUM_HOME}"
+  bootstrapSum "${SUIF_PATCH_SUM_BOOSTSTRAP_BIN}" "" "${SUIF_SUM_HOME}"
+}
+assureSUM
+
+install(){
+  logI "Installing MSR..."
+  export SUIF_INSTALL_INSTALL_DIR=/tmp/MSR
+  export SUIF_SAG_USER_NAME=sag
+  export SUIF_PATCH_AVAILABLE=1
+  export SUIF_SETUP_TEMPLATE_MSR_LICENSE_FILE="${MSRLICENSE_SECUREFILEPATH}"
+  export SUIF_INSTALL_TIME_ADMIN_PASSWORD=manage01
+  export SUIF_ONLINE_MODE=0
+
+  applySetupTemplate "MSR/1011/lean"
+}
+install
+
+containerize(){
+  cd "${SUIF_INSTALL_INSTALL_DIR}/IntegrationServer/docker"
+  ./is_container.sh createLeanDockerfile
+  find "${SUIF_INSTALL_INSTALL_DIR}" -type f -name Dockerfile
+}
+
+finally(){
+  logI "Saving the audit"
+  tar cvzf "$sd/sessions/$crtDay/s_$d.tgz" "${SUIF_AUDIT_BASE_DIR}"
+  logI "Unmounting the shared images folder"
+  sudo umount "$sd"
+  logI "Unmounted, result is $?"
+}
+finally
